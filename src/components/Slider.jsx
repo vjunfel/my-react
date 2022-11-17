83d148b7a7e4595b3961f871179bcd3bb7d1d29a
Junfel <vjunfel@gmail.com>
2022-11-17 18:38:35 +0800

    Add slider page to home page

---
+++ src/components/Slider.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
+import { useNavigate } from "react-router-dom";
+
+export default function Slider() {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function fetchListing() {
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+      console.log(listings);
+    }
+    fetchListing();
+  }, []);
+  if (loading) {
+    return <Spinner />;
+  }
+  if (listings.length === 0) {
+    return <></>;
+  }
+  return (
+    listings && (
+      <>
+        <Swiper
+          slidesPerView={1}
+          navigation
+          pagination={{ type: "progressbar" }}
+          effect="fade"
+          modules={[EffectFade]}
+          autoplay={{ delay: 3000 }}
+        >
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  background: `url(${data.imgUrls[0]}) center, no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="w-full h-[400px] overflow-hidden"
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="">
+      <Slider />
+    </div>
+  );
 }
