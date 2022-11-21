62eb15a4f1de74e692cc5fab3665522e3cda9b2a
Junfel <vjunfel@gmail.com>
2022-11-21 23:35:57 +0800

    Add  places for rent section on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,12 +47,43 @@ export default function Home() {
     fetchListing();
   }, []);
 
+  // Places For Rent
+  const [rentLists, setRentLists] = useState(null);
+  useEffect(() => {
+    async function fetchListing() {
+      try {
+        // Get reference
+        const listingsRef = collection(db, "listings");
+        // Create the query
+        const q = query(
+          listingsRef,
+          where("type", "==", "rent"),
+          orderBy("timestamp", "desc"),
+          limit(5)
+        );
+        // Execute the query
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setRentLists(listings);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchListing();
+  }, []);
+
   return (
     <div>
       <Slider />
       <div className="mt-6 mb-36 sm:mb-24">
         {offerLists && offerLists.length > 0 && (
-          <div className="mb-6 mx-10  border">
+          <div className="mb-6 mx-10">
             <h2 className="px-3 py-0 text-2xl mt-5 font-semibold">
               Recent offers
             </h2>
@@ -63,6 +94,28 @@ export default function Home() {
             </Link>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
               {offerLists.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {rentLists && rentLists.length > 0 && (
+          <div className="  mx-10">
+            <h2 className="px-3 py-0 text-2xl mt-5 font-semibold">
+              Places for rent
+            </h2>
+            <Link to="/category/rent">
+              <p className="px-3 py-2 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for rent
+              </p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {rentLists.map((listing) => (
                 <ListingItem
                   key={listing.id}
                   listing={listing.data}
