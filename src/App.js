d0152062a8f03ac97ebd4966f93b593ef7219309
Junfel <vjunfel@gmail.com>
2022-11-24 18:28:40 +0800

    Add route for category page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
-import Footer from "./components/Footer";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -27,16 +27,16 @@ function App() {
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<SignUp/>} />
           <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing/>} />
           <Route path="/offers" element={<Offers/>} />
+          <Route path="/category/:categoryName" element={<Category/>} />
           <Route path="/create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing/>} />
           </Route>
           <Route path="/edit-listing" element={<PrivateRoute />}>
             <Route path="/edit-listing/:listingId" element={<EditListing/>} />
           </Route>
-          <Route path="/category/:categoryName/:listingId" element={<Listing/>} />
         </Routes>
-        <Footer/>
       </Router>
       <ToastContainer
         position="bottom-center"
@@ -49,7 +49,7 @@ function App() {
         draggable
         pauseOnHover
         theme="dark"
-/>
+      />
     </>
   );
 }
